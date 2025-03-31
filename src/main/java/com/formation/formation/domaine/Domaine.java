2c3ecc9c12ca118a52e82348a65ec713a1ffe5d8
Ahmed Hajjem <hajjemahmed4@gmail.com>
2025-03-31 19:20:40 +0100

    modified domain entity

--- src/main/java/com/formation/formation/domaine/Domaine.java
+++ src/main/java/com/formation/formation/domaine/Domaine.java
@@ -21,11 +21,8 @@ public class Domaine {
 
     public Domaine(String libelle, Set<Formation> formations) {
         this.libelle = libelle;
-        this.formations = formations;
     }
 
-    @OneToMany(mappedBy = "domaine")
-    private Set<Formation> formations;
 
     public String getLibelle() {
         return libelle;
@@ -33,14 +30,6 @@ public class Domaine {
 
     public void setLibelle(String libelle) {
         this.libelle = libelle;
-    }
-
-    public Set<Formation> getFormations() {
-        return formations;
-    }
-
-    public void setFormations(Set<Formation> formations) {
-        this.formations = formations;
     }
 
     public UUID getId() {
